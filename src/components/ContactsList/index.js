935a347251550a82c4b0ff810de33999e6d06c0e
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-02-20 12:28:05 -0300

    first commit

---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,13 @@
+import { Container, Header } from './styles';
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/" aria-label="Novo contato">Novo Contato</a>
+      </Header>
+      ContactsList
+    </Container>
+  );
+}
