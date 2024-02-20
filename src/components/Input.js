ba2775899a82d8813c63e1282ac7d592a0566284
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-02-20 14:36:44 -0300

    implementa componente de input

---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  background: #fff;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+
+  transition: border-color 0.2s ease-in-out;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main}
+  }
+`;
