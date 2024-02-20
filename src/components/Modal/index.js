c97aeb4cbe4126d6c444a798d56073938c35d5e6
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-02-20 15:31:53 -0300

    implementa portal ao modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 
 import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
 
 export default function Modal({ danger }) {
-  return (
+  return ReactDOM.createPortal(
     <Overlay>
       <Container danger={danger}>
         <h1>Título do modal</h1>
@@ -18,7 +19,8 @@ export default function Modal({ danger }) {
           <Button type="button" danger={danger}>Deletar</Button>
         </Footer>
       </Container>
-    </Overlay>
+    </Overlay>,
+    document.getElementById('modal-root'),
   );
 }
 
