e068a0c1be4673e97e995d0255213866cd08ae08
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-02-20 14:37:00 -0300

    adiciona componentes em tela

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,19 @@
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+    </>
   );
 }
