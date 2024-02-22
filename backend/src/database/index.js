6f44ee78d6e7a38969ab23dcafc0de24f328e74f
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-02-22 17:02:58 -0300

    dockerizada aplicacao

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,14 +1,26 @@
 const { Client } = require('pg');
+const { database } = require('../config');
 
-const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'mycontacts',
-});
+const client = new Client(database);
 
 client.connect();
+client.query(`
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+  CREATE TABLE IF NOT EXISTS categories(
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS contacts(
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE,
+    phone VARCHAR,
+    category_id UUID,
+    FOREIGN KEY(category_id) REFERENCES categories(id)
+  );
+`);
 
 exports.query = async (query, values) => {
   const { rows } = await client.query(query, values);
